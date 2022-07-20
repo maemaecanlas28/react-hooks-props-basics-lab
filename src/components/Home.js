86c7dd07db76c48b570752dc5ef83cbe38b0b079
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-20 00:09:08 -0700

    commit tree 0f465412705c822938dd7167369d4a3b54dbcd27

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Home(props) {
+
+function Home({ name, city, color }) {
+const h1Style = {
+  color: color
+}
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={ h1Style }>
+        { name } is a Web Developer from { city }
       </h1>
     </div>
   );
